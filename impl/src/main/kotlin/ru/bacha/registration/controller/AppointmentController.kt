31c14ddba39d5d7979542a79bb1a3bec9b63f692
Bahtiyar_Tleumagambetov <bahtiyar_tleumagambetov@rntgroup.com>
2025-09-16 10:46:11 +0400

    Fix openapi building

--- impl/src/main/kotlin/ru/bacha/registration/controller/AppointmentController.kt
+++ impl/src/main/kotlin/ru/bacha/registration/controller/AppointmentController.kt
@@ -1,13 +1,16 @@
 package ru.bacha.registration.controller
 
 import lombok.RequiredArgsConstructor
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import ru.bacha.api.AppointmentApi
+import ru.bacha.model.AppointmentDto
 import ru.bacha.registration.persistence.model.Appointment
 
 @RestController
 @RequiredArgsConstructor
-class AppointmentController {
-    fun createAppointment(appointment: Appointment): Appointment {
-        return appointment
+class AppointmentController : AppointmentApi {
+    override fun createAppointment(appointmentDto: AppointmentDto?): ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
     }
 }
